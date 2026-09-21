2b411c90d500c7d925f5cf47c13450e6e13b5769
Vinayak Gupta <www.guptavinayak09@gmail.com>
2026-09-21 06:07:39 +0000

    Fix AIOps pipeline imports and event flow

--- src/aiops_pipeline.py
+++ src/aiops_pipeline.py
@@ -1,9 +1,9 @@
 import json
 
-from anomaly_detector import AnomalyDetector
-from event_consumer import EventConsumer
-from event_producer import EventProducer
-from event_topic import EventTopic
+from src.anomaly_detector import AnomalyDetector
+from src.event_consumer import EventConsumer
+from src.event_producer import EventProducer
+from src.event_topic import EventTopic
 
 
 def load_data(file_path):
@@ -14,15 +14,11 @@ def load_data(file_path):
 def run_pipeline(file_path):
     data = load_data(file_path)
 
-    # INTENTIONAL ASSESSMENT ISSUE #2
-    producer_topic = EventTopic("service-events")
+    shared_topic = EventTopic("anomaly-events")
 
     detector = AnomalyDetector()
-    producer = EventProducer(producer_topic)
-
-    # INTENTIONAL ASSESSMENT ISSUE #3
-    consumer_topic = EventTopic("anomaly-events")
-    consumer = EventConsumer(consumer_topic)
+    producer = EventProducer(shared_topic)
+    consumer = EventConsumer(shared_topic)
 
     detected_events = []
 
